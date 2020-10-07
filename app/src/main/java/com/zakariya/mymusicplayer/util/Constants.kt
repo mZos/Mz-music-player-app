ae673ebb4e20c4eb526206c4e696995d6432280e
Mohammad Zakariya <mohdzakariya272@gmail.com>
2020-10-07 18:41:27 +0530

    Added SongRepository.kt

--- app/src/main/java/com/zakariya/mymusicplayer/util/Constants.kt
+++ app/src/main/java/com/zakariya/mymusicplayer/util/Constants.kt
@@ -1,6 +1,17 @@
 package com.zakariya.mymusicplayer.util
 
-const val PREF_NAME = "MyMusicPlayer"
+import android.provider.MediaStore
+
+const val PREF_NAME = "com.zakariya.mymusicplayer.SHARED_PREF"
 const val POSITION_KEY = "position"
 const val REQ_CODE = 0
 const val RES_POS_KEY = "resumePosition"
+
+
+@Suppress("DEPRECATION")
+val baseProjection = arrayOf(
+    MediaStore.Audio.AudioColumns.TITLE,
+    MediaStore.Audio.AudioColumns._ID,
+    MediaStore.Audio.AudioColumns.DATA,
+    MediaStore.Audio.ArtistColumns.ARTIST
+)
