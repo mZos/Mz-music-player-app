5e7875be85d879cbce28661d2a81ca347d1cd1d0
Mohammad Zakariya <mohdzakariya272@gmail.com>
2020-10-11 11:50:55 +0530

    Optimized some code

--- app/src/main/java/com/zakariya/mymusicplayer/ui/fragment/SongFragment.kt
+++ app/src/main/java/com/zakariya/mymusicplayer/ui/fragment/SongFragment.kt
@@ -1,10 +1,8 @@
 package com.zakariya.mymusicplayer.ui.fragment
 
 import android.content.Context
-import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -13,22 +11,13 @@ import com.zakariya.mymusicplayer.adapter.SongsAdapter
 import com.zakariya.mymusicplayer.repository.SongRepository
 import com.zakariya.mymusicplayer.ui.SongViewModel
 import com.zakariya.mymusicplayer.ui.SongViewModelFactory
-import com.zakariya.mymusicplayer.util.Constants.POSITION_KEY
-import com.zakariya.mymusicplayer.util.Constants.PREF_NAME
 import com.zakariya.mymusicplayer.util.OnSongClickListener
 import kotlinx.android.synthetic.main.fragment_song.*
 
-@Suppress("DEPRECATION")
 class SongFragment : Fragment(R.layout.fragment_song) {
 
-    private lateinit var sharedPreferences: SharedPreferences
-
-    companion object {
-        var currentPlyingSongPosition = -1
-    }
-
     private lateinit var viewModel: SongViewModel
-
+    private lateinit var adapter: SongsAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val repository = SongRepository(requireContext())
@@ -36,32 +25,42 @@ class SongFragment : Fragment(R.layout.fragment_song) {
         viewModel = ViewModelProvider(this, viewModelFactory).get(SongViewModel::class.java)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        sharedPreferences = requireActivity().getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
-        setUpRecyclerView()
+    override fun onResume() {
+        super.onResume()
+        viewModel.forceReload()
     }
 
-    private fun setUpRecyclerView() {
-        rvSongs.layoutManager = LinearLayoutManager(activity as Context)
-        val listener = object : OnSongClickListener {
-            override fun onSongClickListener(position: Int, songPath: String) {
-                Toast.makeText(requireContext(), "clicked", Toast.LENGTH_SHORT).show()
-                currentPlyingSongPosition = position
-
-                with(sharedPreferences.edit()) {
-                    putInt(POSITION_KEY, currentPlyingSongPosition)
-                    apply()
-                }
-                requireActivity().supportFragmentManager.beginTransaction()
-                    .replace(R.id.playerFragmentContainer, PlayerFragment())
-                    .commit()
-            }
-        }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initLayoutManager()
+        initAdapter()
 
         viewModel.songLiveData.observe(viewLifecycleOwner, {
-            rvSongs.adapter = SongsAdapter(activity as Context, it, listener)
+            if (it.isNotEmpty()) {
+                adapter.updateSongList(it)
+            } else {
+                adapter.updateSongList(emptyList())
+            }
         })
+    }
+
+    private fun initAdapter() {
+        val listener = object : OnSongClickListener {
+            override fun onSongClickListener(position: Int, songPath: String) {
+                restartPlayerFragment()
+            }
+        }
+        adapter = SongsAdapter(requireContext(), mutableListOf(), listener)
+        rvSongs.adapter = adapter
+    }
+
+    private fun initLayoutManager() {
+        rvSongs.layoutManager = LinearLayoutManager(activity as Context)
+    }
+
+    private fun restartPlayerFragment() {
+        requireActivity().supportFragmentManager.beginTransaction()
+            .replace(R.id.playerFragmentContainer, PlayerFragment())
+            .commit()
     }
 }
