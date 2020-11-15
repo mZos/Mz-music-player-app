28ca7014b7aac4885feaaa9c5e33165fcd22622b
Mohammad Zakariya <mohdzakariya272@gmail.com>
2020-11-15 13:05:29 +0530

    removed log statement
    Signed-off-by: Mohammad Zakariya <mohdzakariya272@gmail.com>

--- app/src/main/java/com/zakariya/mymusicplayer/ui/activity/SplashActivity.kt
+++ app/src/main/java/com/zakariya/mymusicplayer/ui/activity/SplashActivity.kt
@@ -4,7 +4,6 @@ import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
@@ -29,7 +28,6 @@ class SplashActivity : AppCompatActivity() {
             ActivityCompat.requestPermissions(this, permissions, REQ_CODE)
         } else {
             lifecycleScope.launch(Dispatchers.Main) {
-                Log.i("Splash", "llll")
                 delay(100L)
                 Intent(this@SplashActivity, MainActivity::class.java).also {
                     startActivity(it)
