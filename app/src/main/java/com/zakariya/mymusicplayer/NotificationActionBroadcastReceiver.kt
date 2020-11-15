5767283587423d25b5444fe71b6bc5ec210c3150
Mohammad Zakariya <mohdzakariya272@gmail.com>
2020-11-15 10:37:34 +0530

    updated notification actions working
    Signed-off-by: Mohammad Zakariya <mohdzakariya272@gmail.com>

--- app/src/main/java/com/zakariya/mymusicplayer/NotificationActionBroadcastReceiver.kt
+++ app/src/main/java/com/zakariya/mymusicplayer/NotificationActionBroadcastReceiver.kt
@@ -3,29 +3,25 @@ package com.zakariya.mymusicplayer
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.util.Log
-import android.widget.Toast
 import com.zakariya.mymusicplayer.services.ACTION_NEXT
 import com.zakariya.mymusicplayer.services.ACTION_PLAY_PAUSE
 import com.zakariya.mymusicplayer.services.ACTION_PREVIOUS
+import com.zakariya.mymusicplayer.util.MusicPlayerRemote
 
 class NotificationActionBroadcastReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
         if (intent?.action != null) {
             when (intent.action) {
                 ACTION_PREVIOUS -> {
-                    Toast.makeText(context, "Previous", Toast.LENGTH_SHORT).show()
-                    Log.i("Broadcast", "previous")
+                    MusicPlayerRemote.playPreviousSong()
                 }
 
                 ACTION_PLAY_PAUSE -> {
-                    Toast.makeText(context, "Play Pause", Toast.LENGTH_SHORT).show()
-                    Log.i("Broadcast", "play pause")
+                    MusicPlayerRemote.playPause()
                 }
 
                 ACTION_NEXT -> {
-                    Toast.makeText(context, "Next", Toast.LENGTH_SHORT).show()
-                    Log.i("Broadcast", "next")
+                    MusicPlayerRemote.playNextSong()
                 }
 
                 else -> Unit
