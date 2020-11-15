768a0df5411cfd69d86c4a697ed6a1894d8d5dab
Mohammad Zakariya <mohdzakariya272@gmail.com>
2020-11-15 21:54:24 +0530

    added more constants
    Signed-off-by: Mohammad Zakariya <mohdzakariya272@gmail.com>

--- app/src/main/java/com/zakariya/mymusicplayer/util/Constants.kt
+++ app/src/main/java/com/zakariya/mymusicplayer/util/Constants.kt
@@ -4,12 +4,14 @@ import android.provider.MediaStore
 
 object Constants {
 
-    const val PREF_NAME = "com.zakariya.mymusicplayer.SHARED_PREF"
-    const val POSITION_KEY = "position"
+    private const val packageName = "com.zakariya.mymusicplayer"
+    const val PREF_NAME = "$packageName.SHARED_PREF"
+    const val POSITION_KEY = "${packageName}.position"
+    const val CURRENT_SONG_DURATION_KEY = "$packageName.currentSongDurationKey"
     const val SAVE_CURRENT_SONG_KEY = "Save currently playing song"
     const val REQ_CODE = 0
-    const val NOTIFICATION_CHANNEL_ID = "Music Player"
-    const val NOTIFICATION_CHANNEL_NAME = "Music"
+    const val NOTIFICATION_CHANNEL_ID = "${packageName}.Music Player"
+    const val NOTIFICATION_CHANNEL_NAME = "${packageName}.Music"
     const val NOTIFICATION_ID = 1
 
     @Suppress("DEPRECATION")
