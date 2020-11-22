5f7498995f4fe1e3c43d372a9d7c1e1f9ba6c973
Mohammad Zakariya <mohdzakariya272@gmail.com>
2020-11-22 15:14:53 +0530

    initial commit
    Signed-off-by: Mohammad Zakariya <mohdzakariya272@gmail.com>

--- app/src/main/java/com/zakariya/mymusicplayer/services/MediaSessionCallback.kt
+++ app/src/main/java/com/zakariya/mymusicplayer/services/MediaSessionCallback.kt
@@ -2,17 +2,11 @@ package com.zakariya.mymusicplayer.services
 
 import android.content.Context
 import android.support.v4.media.session.MediaSessionCompat
-import android.util.Log
 
 class MediaSessionCallback(private val context: Context, private val service: PlayerService) :
     MediaSessionCompat.Callback() {
     override fun onSeekTo(pos: Long) {
         super.onSeekTo(pos)
-        iLog("seek")
         service.seekTo(pos.toInt())
-    }
-
-    private fun iLog(msg: String) {
-        Log.i(this::class.java.simpleName, msg)
     }
 }
